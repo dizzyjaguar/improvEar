c5836bf1c9317da81a74c294701cf45f4040bf70
Eli Zevin <dizzyjaguar@gmail.com>
2021-02-09 11:16:08 -0800

    succesfully playing a chord

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import * as Tone from "tone";
 
 const Home = () => {
+  const synth = new Tone.PolySynth().toDestination();
+  
+  const playChord = (chord) => {
+    synth.triggerAttackRelease(chord, 1);
+  };
+
+  synth.set({ detune: -1200 });
+
+  const cmajor7 = ['C4', 'E4', 'G4']
+
   return (
     <>
+      
+        <button className="note" onClick={() => playChord(cmajor7)}>
+          Cmaj7
+        </button>
       
     </>
   )
