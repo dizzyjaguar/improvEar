d20530322e1a1db44ec7c8978b352a9d9d35e2c4
Eli Zevin <dizzyjaguar@gmail.com>
2021-02-12 11:33:31 -0800

    fixed html option element value coming back as a string, now just using it for reference rather than setting state to the value, since we need an array of intervals not a string

--- src/components/Chord.jsx
+++ src/components/Chord.jsx
@@ -7,21 +7,17 @@ const Chord = () => {
   const [keyCenter, setKeyCenter] = useState('C')
   const [octave, setOctave] = useState(4)
   const [chordType, setChordType] = useState([0, 4, 7])
-  const [currentChord, setCurrentChord] = useState()
+  // const [currentChord, setCurrentChord] = useState()
 
   
   const synth = new Tone.PolySynth().toDestination();
   
 
   const playChord = () => {
-    synth.triggerAttackRelease(Tone.Frequency(keyCenter.concat(octave.toString())).harmonize(chordType), 1);
+    const startingNote = keyCenter.concat(octave.toString())
+    console.log(startingNote)
+    synth.triggerAttackRelease(Tone.Frequency(startingNote).harmonize(chordType), 1);
   };
-  
-  //probably dont need this now 
-  // const changeKey = (chord, interval) => {
-  //   const newChord = chord.map(note => Tone.Frequency(note).transpose(interval).toNote())
-  //   setKeyCenter(newKey)
-  // };
 
   const keyNodes = keyCenters.map(key => {
     return <option key={key.name} value={key.value}>{key.name}</option>
@@ -39,28 +35,23 @@ const Chord = () => {
     setKeyCenter(value);
   };
 
-  // const handleChordTypeChange = (event) => {
-  //   const { target } = event;
-  //   const { value } = target;
-  //   event.persist();
-
-  //   setCurrentChordType(value);
-  // };
-
-  // useEffect(() => {
-  //   setCurrentChord(chordTypes[currentChordType])
-  // }, [currentChordType])
-
-
+  const handleChordTypeChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    event.persist();
+    
+    setChordType(chordTypes[value]);
+  };
+  
   return (
     <>
       <select id="keys" name="keys" onChange={(handleKeyChange)}>
         {keyNodes}
       </select>
-      {/* <button onClick={() => changeKey(currentChord, keyCenter)} >Change Key</button> */}
-      {/* <select id="chords" name="chords" onChange={(handleChordTypeChange)}>
+      
+      <select id="chords" name="chords" onChange={(handleChordTypeChange)}>
         {chordNodes}
-      </select> */}
+      </select>
       {/* <p>{currentChord[0].slice(0, -1)}  {currentChordType}</p> */}
       <button className="note" onClick={() => playChord()}>
           Playchord
