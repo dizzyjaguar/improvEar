3a229a74d519bb6db67b6602974a28c9bb946ad8
Eli Zevin <dizzyjaguar@gmail.com>
2021-02-14 10:26:43 -0800

    start building basic sampler, which will run the scale, could also use sampler for chords too later if sounds better

--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Tone from "tone";
+
 
 const Scale = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  const pianoSampler = new Tone.Sampler({
+    urls: {
+      "C4": "C4.mp3",
+      "D#4": "Ds4.mp3",
+      "F#4": "Fs4.mp3",
+      "A4": "A4.mp3",
+    },
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
+  }).toDestination();
+  
+  Tone.loaded().then(() => {
+    setIsLoaded(true)
+  })
+
+  const handleClick = () => pianoSampler.triggerAttack("C4");
+  
   return (
     <>
+      {
+        isLoaded ? <button disabled={!isLoaded} onClick={handleClick}>click</button>
+        : <p>loading...</p>
+      }
+      
     add a scale selector and a button to play the chord
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Chord from '../components/Chord';
+import Scale from '../components/Scale';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <>
       {/* add a button to play both the scale and chord at the same time */}
         <Chord />
+        <Scale />
       
     </>
   )
