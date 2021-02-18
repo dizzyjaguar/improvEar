ca2e6dad6315465d8a16bc2880a677769eabb3eb
Eli Zevin <dizzyjaguar@gmail.com>
2021-02-18 11:15:02 -0800

    transport succesfully displaying with constant render

--- src/components/Metronome.jsx
+++ src/components/Metronome.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as Tone from "tone";
 
 
 const Metronome = () => {
   const [tempoState, setTempoState] = useState(100)
-  const now = Tone.now()
+  const [bars, setBars] = useState(0)
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBars(Tone.Transport.position);
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
+  
+
   const theTempo = Tone.Transport.bpm.value = tempoState;
   
   return (
     <>
       <h3>Tempo</h3>
       <p>{theTempo}</p>
+      <p>{bars}</p>
       <button onClick={() => setTempoState(tempoState - 1)}>lower</button>
       <button onClick={() => setTempoState(tempoState + 1)}>raise</button>
+      <button onClick={() => Tone.Transport.start()}>start</button>
+      <button onClick={() => Tone.Transport.stop()}>stop</button>
     </>
   )
 }
--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -24,7 +24,7 @@ const Scale = () => {
   })
 
 
-  const scale1 = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4']
+  const scale1 = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
 
   const testSequence = new Tone.Sequence((time, note) => {
     pianoSampler.triggerAttackRelease(note, 0.1, time);
