611e418900748945cbac377db6591303ef64cdb4
Eli Zevin <dizzyjaguar@gmail.com>
2021-02-18 12:24:12 -0800

    some clean up

--- src/components/Chord.jsx
+++ src/components/Chord.jsx
@@ -31,7 +31,7 @@ const Chord = () => {
   const playChord = () => {
     const startingNote = keyCenter.concat(octave.toString())
     console.log(startingNote)
-    pianoSampler.triggerAttackRelease(Tone.Frequency(startingNote).harmonize(chordType), "1n");
+    pianoSampler.triggerAttackRelease(Tone.Frequency(startingNote).harmonize(chordType), "1n", Tone.now(), 2.5 );
   };
 
   // need to get the chord to start with the transport just like the scale
@@ -66,18 +66,23 @@ const Chord = () => {
   return (
     <>
       <span>Ocatave</span>
-      <button onClick={() => setOctave(octave + 1)}>Up</button>
-      <button onClick={() => setOctave(octave - 1)}>Down</button>
-      <select id="keys" name="keys" onChange={(handleKeyChange)}>
-        {keyNodes}
-      </select>
-      <select id="chords" name="chords" onChange={(handleChordTypeChange)}>
-        {chordNodes}
-      </select>
-      {/* <p>{currentChord[0].slice(0, -1)}  {currentChordType}</p> */}
-      <button className="note" onClick={() => playChord()}>
-          PlayChord
-      </button>
+      {
+        isLoaded ? 
+        <>
+          <button onClick={() => setOctave(octave + 1)}>Up</button>
+          <button onClick={() => setOctave(octave - 1)}>Down</button>
+          <select id="keys" name="keys" onChange={(handleKeyChange)}>
+            {keyNodes}
+          </select>
+          <select id="chords" name="chords" onChange={(handleChordTypeChange)}>
+            {chordNodes}
+          </select>
+          <button className="note" onClick={() => playChord()}>
+              PlayChord
+          </button> 
+        </>
+        : <p>loading...</p>
+      }
     </>
   )
 }
