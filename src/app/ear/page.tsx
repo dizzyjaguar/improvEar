023c184d203b5f14375458626d824eb8afb9e98c
Eli Zevin <dizzyjaguar@gmail.com>
2023-07-22 16:54:36 -0700

    Move over single page to app and convert to tsx

--- src/app/ear/page.tsx
+++ src/app/ear/page.tsx
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
-import Chord from '../components/Chord';
-import Scale from '../components/Scale';
-import { Button } from 'react-bootstrap';
-import * as Tone from "tone";
-import Metronome from '../components/Metronome';
-import { scaleTypes } from '../data/scales';
-
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import * as Tone from 'tone'
+import Chord from '@/components/Chord'
+import Scale from '@/components/Scale'
+import Metronome from '@/components/Metronome'
+import { scaleTypes } from '@/data/scales'
 
 const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false)
   const [scaleKeyCenter, setScaleKeyCenter] = useState('C')
   const [scaleOctave, setScaleOctave] = useState(4)
   const [scaleType, setScaleType] = useState(scaleTypes.major)
-  const scaleStartingNote = scaleKeyCenter.concat(scaleOctave.toString());
-  const selectedScale = Tone.Frequency(scaleStartingNote).harmonize(scaleType);
+  const scaleStartingNote = scaleKeyCenter.concat(scaleOctave.toString())
+  const selectedScale = Tone.Frequency(scaleStartingNote).harmonize(scaleType)
   const scaleLength = selectedScale.length
-
 
   const pianoSampler = new Tone.Sampler({
     urls: {
-      "C4": "C4.mp3",
-      "D#4": "Ds4.mp3",
-      "F#4": "Fs4.mp3",
-      "A4": "A4.mp3",
+      C4: 'C4.mp3',
+      'D#4': 'Ds4.mp3',
+      'F#4': 'Fs4.mp3',
+      A4: 'A4.mp3',
     },
     release: 1,
-    baseUrl: "https://tonejs.github.io/audio/salamander/",
-  }).toDestination();
-
+    baseUrl: 'https://tonejs.github.io/audio/salamander/',
+  }).toDestination()
 
   Tone.loaded().then(() => {
     setIsLoaded(true)
   })
-  
+
   const handleClick = () => {
     // added this because wasn't playing on initial load
     Tone.start()
@@ -43,29 +40,24 @@ const Home = () => {
     Tone.Transport.stop()
   }
 
-  const handleScaleOctave = (direction) => {
+  const handleScaleOctave = (direction: 'up' | 'down') => {
     if (direction === 'up') {
       setScaleOctave(scaleOctave + 1)
     } else if (direction === 'down') {
       setScaleOctave(scaleOctave - 1)
     }
-  };
-  
+  }
+
   // add in some animation for colors corresponding to different scales and chords to make a cool animation when the scale over the chord is played
   return (
     <>
-        {    
-          isLoaded ? <>
-            <Metronome
-              scaleLength={scaleLength}
-            />
-            <br />
-          <Chord 
-            pianoSampler={pianoSampler}
-            scaleLength={scaleLength}
-            />
+      {isLoaded ? (
+        <>
+          <Metronome scaleLength={scaleLength} />
           <br />
-          <Scale 
+          <Chord pianoSampler={pianoSampler} scaleLength={scaleLength} />
+          <br />
+          <Scale
             pianoSampler={pianoSampler}
             keyCenter={scaleKeyCenter}
             setKeyCenter={setScaleKeyCenter}
@@ -76,17 +68,24 @@ const Home = () => {
             handleScaleOctave={handleScaleOctave}
             octave={setScaleOctave}
           />
-          <br/>
+          <br />
           <p>----------</p>
-          <Button variant="outline-primary" disabled={!isLoaded} onClick={handleClick}>Play</Button>
-          <Button variant="outline-primary" onClick={handleStop}>Stop</Button>
-          </>
-          : <p>loading...</p>
-        }
-      
+          <Button
+            variant="outline-primary"
+            disabled={!isLoaded}
+            onClick={handleClick}
+          >
+            Play
+          </Button>
+          <Button variant="outline-primary" onClick={handleStop}>
+            Stop
+          </Button>
+        </>
+      ) : (
+        <p>loading...</p>
+      )}
     </>
   )
 }
 
-
-export default Home;
+export default Home
