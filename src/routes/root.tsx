0d36260796e5f05b905be6c104d6164e686d6189
Eli Zevin <dizzyjaguar@gmail.com>
2023-10-22 19:25:48 -0700

    Add h3 styling

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,9 +4,10 @@ export default function Root() {
       className={`w-full h-screen p-10 rainbow-gradient flex justify-center items-center`}
     >
       <div
-        className={`bg-white w-screen max-w-2xl h-3/5 rounded-md shadow-md pt-10`}
+        className={`bg-white w-screen max-w-2xl h-3/5 rounded-md shadow-md pt-16`}
       >
-        <h1 className={`text-center font-bold h1`}>Improve your Ear</h1>
+        <h1 className={`text-center h1 mb-8`}>Improve your Ear</h1>
+        <h3 className={`h3 text-center`}>Go ahead and take a listen</h3>
       </div>
     </div>
   )
