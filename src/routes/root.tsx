a3946aa947a2b7ea8408aec8b9d72c458697eff5
Eli Zevin <dizzyjaguar@gmail.com>
2023-10-22 19:40:11 -0700

    Add icons to landing page

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,7 @@
+import Air from '../components/icons/Air'
+import Sun from '../components/icons/Sun'
+import Water from '../components/icons/Water'
+
 export default function Root() {
   return (
     <div
@@ -7,7 +11,12 @@ export default function Root() {
         className={`bg-white w-screen max-w-2xl h-3/5 rounded-md shadow-md pt-16`}
       >
         <h1 className={`text-center h1 mb-8`}>Improve your Ear</h1>
-        <h3 className={`h3 text-center`}>Go ahead and take a listen</h3>
+        <h3 className={`h3 text-center mb-6`}>Go ahead and take a listen</h3>
+        <div className={`w-full flex justify-center items-center space-x-4`}>
+          <Sun />
+          <Water />
+          <Air />
+        </div>
       </div>
     </div>
   )
