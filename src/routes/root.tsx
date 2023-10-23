37ac5967aa87b61a8c2defd7ad877cabb6e1c8a3
Eli Zevin <dizzyjaguar@gmail.com>
2023-10-22 18:42:20 -0700

    Setup initial tailwind theme

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,13 @@
 export default function Root() {
-  return <div className={`w-full h-screen`}></div>
+  return (
+    <div
+      className={`w-full h-screen p-10 rainbow-gradient flex justify-center items-center`}
+    >
+      <div
+        className={`bg-white w-screen max-w-2xl h-3/5 rounded-md shadow-md pt-10`}
+      >
+        <h1 className={`text-center font-bold h1`}>Improve your Ear</h1>
+      </div>
+    </div>
+  )
 }
