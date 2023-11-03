eea979a3209c9d89d728294b68498bf460c04c91
Eli Zevin <dizzyjaguar@gmail.com>
2023-11-03 16:24:31 -0700

    Add animation to landing page text

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,31 +1,41 @@
 import { useNavigate } from 'react-router-dom'
 import Button from '../Button'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Landing() {
   const navigate = useNavigate()
 
   return (
-    <motion.div>
-      <motion.h1
-        className={`text-center h1 mb-8`}
+    <AnimatePresence mode='sync'>
+      <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 1 } }}
-        exit={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.5 } }}
       >
-        Improve your Ear
-      </motion.h1>
-      <h3 className={`h3 text-center mb-6`}>Go ahead and take a listen</h3>
-      <div className={`h-[33px] mb-20`}></div>
-      <div className={`w-full flex justify-center items-center`}>
-        <Button
-          onClick={() =>
-            navigate('about', { state: { iconLocation: 'top left' } })
-          }
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.8 } }}
+          className={`text-center h1 mb-8`}
         >
-          Start
-        </Button>
-      </div>
-    </motion.div>
+          Improve your Ear
+        </motion.h1>
+        <motion.h3
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1.2 } }}
+          className={`h3 text-center mb-6`}
+        >
+          Go ahead and take a listen
+        </motion.h3>
+        <div className={`h-[33px] mb-20`}></div>
+        <div className={`w-full flex justify-center items-center`}>
+          <Button
+            onClick={() =>
+              navigate('about', { state: { iconLocation: 'top left' } })
+            }
+          >
+            Start
+          </Button>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
