82106e5e4cf516f61e550d5c853f8e8d199c191b
Eli Zevin <dizzyjaguar@gmail.com>
2023-11-10 10:57:08 -0800

    Improve about page animation

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,44 @@
+import { AnimatePresence, motion } from 'framer-motion'
+
+const visible = { opacity: 1, transition: { duration: 2.5 } }
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  visible,
+}
+
 export default function About() {
-  return <h1>About</h1>
+  return (
+    <>
+      <motion.div
+        className={`pt-10 pl-10 pr-4 sm:pl-20`}
+        initial="hidden"
+        animate="visible"
+        variants={{ visible: { transition: { staggerChildren: 1 } } }}
+      >
+        <motion.h2 variants={itemVariants} className={`h2 mb-10 text-left`}>
+          ImprovEar is a tool for musicians <br /> who want to improve their{' '}
+          <motion.span className="text-cornflower-lilac-300">ear</motion.span>
+        </motion.h2>
+        <motion.h3
+          variants={itemVariants}
+          className={` pl-8 sm:pl-16 h3 text-left`}
+        >
+          Inspired by{' '}
+          <motion.span className="text-calico-300">beautiful</motion.span>{' '}
+          devices <br /> that exist in the physical world.
+        </motion.h3>
+
+        <motion.p
+          variants={itemVariants}
+          className={`mt-10 quote leading-[2rem] w-2/3`}
+        >
+          “Music is your own experience, your own thoughts, your wisdom. If you
+          don’t live it, it won’t come out of your horn. They teach you there’s
+          a boundary line to music. But, man, there’s no boundary line to art” –
+          Charlie Parker
+        </motion.p>
+      </motion.div>
+    </>
+  )
 }
