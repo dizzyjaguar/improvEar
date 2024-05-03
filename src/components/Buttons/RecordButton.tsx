18e7b85d0a1527772ae2d04b3b707e6b355e1232
Eli Zevin <dizzyjaguar@gmail.com>
2024-05-03 08:52:22 -0700

    finish v1 record button ui

--- src/components/Buttons/RecordButton.tsx
+++ src/components/Buttons/RecordButton.tsx
@@ -1,17 +1,11 @@
 import { motion } from 'framer-motion'
 import Record from '../icons/Record'
-import { ButtonHTMLAttributes } from 'react'
 
 export default function RecordButton(onClick: any) {
   return (
-    <motion.button
-      // backgroundColor={'transparent'}
-      // border={'2px solid black'}
-      // height={'48px'}
-      // width={'48px'}
-      // _hover={{ backgroundColor: 'transparent', borderWidth: '1px', opacity: .8 }}
-      className={`hover:opacity-50`}
-      whileHover={{ scale: 1.1, rotate: 180 }}
+    <motion.button   
+      className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
+      whileHover={{ scale: 1.1, rotate: 45 }}
       onClick={onClick}
     >
       <Record />
