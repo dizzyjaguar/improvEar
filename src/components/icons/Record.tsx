49022664bb7a8aa1296a5d47b569b0005f835933
Eli Zevin <dizzyjaguar@gmail.com>
2024-05-02 20:33:05 -0700

    create record icon

---
+++ src/components/icons/Record.tsx
@@ -0,0 +1,15 @@
+import { motion } from 'framer-motion'
+
+export default function Record() {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle cx="12" cy="12" r="12" fill="#F35E06" />
+    </motion.svg>
+  )
+}
