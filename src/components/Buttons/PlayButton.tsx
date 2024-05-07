565b5b3832927a93058ce622cbbb48bf52114b86
mouakiang <mouakkia@gmail.com>
2024-05-07 12:33:25 -0400

    Updated play-button animation to move right instead of rotating

--- src/components/Buttons/PlayButton.tsx
+++ src/components/Buttons/PlayButton.tsx
@@ -5,7 +5,7 @@ export default function PlayButton(onClick: any) {
   return (
     <motion.button
       className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
-      whileHover={{ scale: 1.1, rotate: 45 }}
+      whileHover={{ scale: 1.1, x: 3 }}
       onClick={onClick}
     >
       <Play />
