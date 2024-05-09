7277b40bb1a0c0a2c289bd7f54acd8964450b9f2
mouakiang <mouakkia@gmail.com>
2024-05-09 13:53:46 -0400

    Added play-button and animation ui

---
+++ src/components/Buttons/PlayButton.tsx
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion'
+import Play from '../icons/Play'
+
+export default function PlayButton(onClick: any) {
+  return (
+    <motion.button
+      className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
+      whileHover={{ scale: 1.1, rotate: 45 }}
+      onClick={onClick}
+    >
+      <Play />
+    </motion.button>
+  )
+}
