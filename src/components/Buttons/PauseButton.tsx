34af036c088fe6978c6ae78ef721dc6d23455a2a
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2024-05-20 12:57:05 -0400

    Added pause button

---
+++ src/components/Buttons/PauseButton.tsx
@@ -0,0 +1,15 @@
+import { motion } from 'framer-motion'
+import Pause from '../icons/Pause'
+
+export default function PauseButton(onClick: any) {
+  return (
+    <button
+      className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
+      onClick={onClick}
+    >
+      <motion.div whileHover={{ scale: 1.1 }}>
+        <Pause />
+      </motion.div>
+    </button>
+  )
+}
