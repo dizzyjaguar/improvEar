19d84cf7438d577cb9ddb8ea3815167a3d5523a8
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2024-05-20 11:53:34 -0400

    This reverts commit 575220f1333e1745e91df80d765c8f3098ad9912, reversing changes made to 732ed062dd2da10bc17ae224c3f5fa34692d38ed.

--- src/components/Buttons/PlayButton.tsx
+++ src/components/Buttons/PlayButton.tsx
@@ -7,7 +7,7 @@ export default function PlayButton(onClick: any) {
       className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
       onClick={onClick}
     >
-      <motion.div whileHover={{x: 4 }}>
+      <motion.div whileHover={{ scale: 1.4, x: 6 }}>
         <Play />
       </motion.div>
     </button>
