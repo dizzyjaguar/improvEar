d94da55ad8b2716013addf77def93a4e23cc5dfa
mouakiang <mouakkia@gmail.com>
2024-06-18 14:15:40 -0400

    Updated spacing and sizing on indicator menus

--- src/components/Indicator/index.tsx
+++ src/components/Indicator/index.tsx
@@ -11,12 +11,26 @@ export default function Indicator({
 }: IndicatorProps) {
   return (
     <button>
-      <div className={`space-y-1 bg-black hover:bg-alabaster-900 p-2 rounded-lg`}>
-        <h2 className={`h2 text-white uppercase text-left`}>{variant}</h2>
-        <div className={`flex flex-row space-x-2 items-center`}>
-          <p className={`text-white p`}>{settingOne}</p>
+      <div
+        className={`space-y--1 bg-black hover:bg-alabaster-900 py-2 px-3 rounded-lg md:px-8 lg:px-8`}
+      >
+        <h2
+          className={`h2 text-white uppercase text-left font-semibold text-sm sm:text-base md:text-3xl lg:text-3xl`}
+        >
+          {variant}
+        </h2>
+        <div className={`flex flex-row space-x-2 items-center justify-start`}>
+          <p
+            className={`text-white p text-xs sm:text-sm md:text-lg lg:text-2xl`}
+          >
+            {settingOne}
+          </p>
           <div className={`bg-alabaster-100 p-1 rounded`}>
-            <p className={`text-black p`}>{settingTwo}</p>
+            <p
+              className={`text-black p text-xs sm:text-sm md:text-lg lg:text-xl`}
+            >
+              {settingTwo}
+            </p>
           </div>
         </div>
       </div>
