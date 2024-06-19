a9e16839d1ac64bf1a08175e5f66577b84c79a92
mouakiang <mouakkia@gmail.com>
2024-06-19 15:02:45 -0400

    Updated animations for Pause and Play Button

--- src/components/Buttons/PauseButton.tsx
+++ src/components/Buttons/PauseButton.tsx
@@ -1,15 +1,21 @@
+'use client'
 import { motion } from 'framer-motion'
 import Pause from '../icons/Pause'
+import { useState } from 'react'
 
-export default function PauseButton(onClick: any) {
+export default function PauseButton({ onClick }: { onClick: any }) {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
-    <button
-      className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
+    <motion.button
+      className="hover:opacity-50 border-[1px] border-black p-3 rounded"
       onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <motion.div whileHover={{ scale: 1.1 }}>
+      <motion.div animate={{ scale: isHovered ? 1.1 : 1 }}>
         <Pause />
       </motion.div>
-    </button>
+    </motion.button>
   )
 }
--- src/components/Buttons/PlayButton.tsx
+++ src/components/Buttons/PlayButton.tsx
@@ -1,15 +1,21 @@
+'use client'
 import { motion } from 'framer-motion'
 import Play from '../icons/Play'
+import { useState } from 'react'
 
 export default function PlayButton(onClick: any) {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
     <motion.button
-      className="hover:opacity-50 border-[1px] border-black p-3 rounded"
+      className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
       onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <motion.div whileHover={{x: 4 }}>
+      <motion.div animate={{ x: isHovered ? 4 : 1 }}>
         <Play />
       </motion.div>
     </motion.button>
-  );
+  )
 }
