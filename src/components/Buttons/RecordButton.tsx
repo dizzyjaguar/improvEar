e1c966e579942d36beafbcb669011c16f139ac4b
mouakiang <mouakkia@gmail.com>
2024-06-19 15:08:54 -0400

    Destructured onclick prop for buttons

--- src/components/Buttons/RecordButton.tsx
+++ src/components/Buttons/RecordButton.tsx
@@ -1,9 +1,9 @@
 import { motion } from 'framer-motion'
 import Record from '../icons/Record'
 
-export default function RecordButton(onClick: any) {
+export default function RecordButton({ onClick }: { onClick: any }) {
   return (
-    <motion.button   
+    <motion.button
       className={`hover:opacity-50 border-[1px] border-black p-3 rounded`}
       whileHover={{ scale: 1.1, rotate: 45 }}
       onClick={onClick}
